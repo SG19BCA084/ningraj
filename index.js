ade0e43f8141d5da3b13d6beca23a25cb1c48c72
SG19BCA084 <ningarajingalgi98@gmail.com>
2022-05-20 11:28:21 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+function rahul (){
+    const age= prompt("enter yourage");
+    if (age>18
+    alert("you can drive")  
+}
+
+else{
+alert("you cant drive ")
+}
---
+++ inter.js
@@ -0,0 +1,12 @@
+function  ningu(){
+    const age=prompt("enetr your age");
+    if (age>18){
+        alert("you can drive");s
+    }
+    else if(age>=11 && age<20){
+    alert("yiu can go the high school");
+    }
+    else if(age>=20){
+        alert("you can go anywhere");
+    }
+}
